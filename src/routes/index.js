eab9f350499a839b57230091ef9875539e24e5ac
Pablo Benedix <pabling.benedix@gmail.com>
2021-12-09 14:53:06 -0300

    Bug en actualizar

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const fun = require('../controller/controller.js')
 
+const viewUrl = "_design/all_pizzas/_view/all";
+const viewUrlUsers = "_design/all_pizzas/_view/empleados";
+
 
 // conexion a la base de datos
 
@@ -29,7 +32,6 @@ const dbName = "restaurant";
 
 
 router.get('/', (req, res) => {
-    const viewUrl = "_design/all_pizzas/_view/all";
     fun.Listar(res, 'index', {}, viewUrl);
 })
 
@@ -59,10 +61,11 @@ router.get('/actualizar/:id/:rev', (req, res) => {
     idRes = req.params.id;
     revRes = req.params.rev;
 
-    fun.Listar(res, 'actualizar', { title: 'Actualizar un registro', id: idRes, rev: revRes})
+    fun.Listar(res, 'actualizar', { title: 'Actualizar un registro', id: idRes, rev: revRes}, viewUrl)
 })
 
 router.post('/actualizar/:id/:rev', (req, res) => {
+    
 
     let updateDocument = {
         id: req.body.id,
@@ -88,8 +91,7 @@ router.get('/borrar/:id/:rev', (req, res) => {
 })
 
 router.get('/empleado', (req, res) => {
-    var viewUrl1 = "_design/all_pizzas/_view/empleados"
-    fun.Listar(res, 'empleado', {} ,viewUrl1);
+    fun.Listar(res, 'empleado', {}, viewUrlUsers);
 })
 
 module.exports = router;
